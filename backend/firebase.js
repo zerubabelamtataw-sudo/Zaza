7e765359e069498af1bb149e7de9c8adccbae393
zerubabelamtataw-sudo <zerubabelamtataw@gmail.com>
2026-09-14 21:55:03 +0300

    Create firebase.js

---
+++ backend/firebase.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const admin = require('firebase-admin');
+
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  serviceAccount = require('./serviceAccountKey.json');
+}
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_DATABASE_URL
+  });
+}
+
+module.exports = admin.database();
